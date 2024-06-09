1963d8231b55bf9b9f77928ef13dd8c03a6e761d
Devon Flynn <Devon.k.flynn@gmail.com>
2024-06-09 16:33:57 -0400

    Updated EnemyAI
    Added a raycast system that checks for objects between enemy and player. Enemy will not see player unless raycast reaches player without interruption.

--- Shadow Sentinel/Assets/Scripts/EnemyFOVCheck.cs
+++ Shadow Sentinel/Assets/Scripts/EnemyFOVCheck.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class EnemyFOVCheck : MonoBehaviour
 {
+    [SerializeField] GameObject rayStartPoint;
+
+    [SerializeField] int visDistance;
+
+    [SerializeField] Vector3 offset;
+
+    private bool playerCheck;
     public bool playerSeen;
 
     // Start is called before the first frame update
@@ -15,14 +22,18 @@ public class EnemyFOVCheck : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (playerCheck) 
+        {
+            CheckRay();
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        
         if (other.gameObject.tag == "Player")
         {
-            playerSeen = true;
+            playerCheck = true;
         }
     }
 
@@ -30,7 +41,29 @@ public class EnemyFOVCheck : MonoBehaviour
     {
         if (other.gameObject.tag == "Player")
         {
+            playerCheck = false;
             playerSeen = false;
+        }
+    }
+
+    private void CheckRay()
+    {
+        RaycastHit hit;
+
+        Debug.DrawRay(rayStartPoint.transform.position, GameManager.instance.player.transform.position - rayStartPoint.transform.position, Color.red);
+
+        if (Physics.Raycast(rayStartPoint.transform.position, GameManager.instance.player.transform.position - rayStartPoint.transform.position, out hit))
+        {
+            Debug.Log(hit.transform.name);
+
+            if (hit.transform.tag == "Player")
+            {
+                playerSeen = true;
+            }
+            else if (hit.transform.tag == "Obstacle")
+            {
+                playerSeen = false;
+            }
         }
     }
 }
