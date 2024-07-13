3ea8582ebc927e05c8270763655d14839fe29cd1
Michael Dunlap <148809895+michaeldunlap96@users.noreply.github.com>
2024-07-13 13:45:35 -0700

    Revert "Merge branch 'main' of https://github.com/Devonflynn96/Shadow-Sentinel"
    This reverts commit 7dc4bd341365da8343e523b525ce2e4718da9d7a.

--- Shadow Sentinel/Assets/Scripts/PickUp.cs
+++ Shadow Sentinel/Assets/Scripts/PickUp.cs
@@ -5,45 +5,20 @@ using UnityEngine;
 public class PickUp : MonoBehaviour
 {
     [SerializeField] gunStats gun;
-    private bool isPlayerNearby;
 
     void Start()
     {
-        GameManager.instance.pickUpMessage.SetActive(false); // Ensure the message is hidden initially
+        
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            isPlayerNearby = true;
-            GameManager.instance.pickUpMessage.SetActive(true); // Show the "Press E" message
+            gun.ammoCur = gun.ammoMax;
+            InventoryManager.instance.AddToInventory(gun);
+            GameManager.instance.playerScript.GetGunStats(gun);
+            Destroy(gameObject);
         }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            isPlayerNearby = false;
-            GameManager.instance.pickUpMessage.SetActive(false); // Hide the "Press E" message
-        }
-    }
-
-    void Update()
-    {
-        if (isPlayerNearby && Input.GetKeyDown(KeyCode.E))
-        {
-            PickUpGun();
-        }
-    }
-
-    void PickUpGun()
-    {
-        gun.ammoCur = gun.ammoMax;
-        InventoryManager.instance.AddToInventory(gun);
-        GameManager.instance.playerScript.GetGunStats(gun);
-        GameManager.instance.pickUpMessage.SetActive(false);
-        Destroy(gameObject);
     }
 }
